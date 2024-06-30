362f413cef00f02eb9b59efc8eb1dcce4f091ac7
mar-alv <mar.alv.cod@gmail.com>
2024-06-30 18:43:45 -0300

    feat: server, middleware and utils created

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import http from 'node:http'
+
+import { extractQueryParams } from './utils/extract-query-params.js'
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+
+const server = http.createServer(async (req, res) => {
+	const { method, url } = req
+
+	await json(req, res)
+ 
+	const route = routes.find(i =>
+		i.method === method && i.path.test(url)
+	)
+
+	if (!route) {
+		return res.writeHead(404).end('route not found')
+	}
+
+	const routeParams = url.match(route.path)
+
+	const { query, ...params } = routeParams.groups
+
+	req.params = params
+	req.query = query ? extractQueryParams(query) : {}
+
+	return route.handler(req, res)
+})
+
+server.listen(3001)
